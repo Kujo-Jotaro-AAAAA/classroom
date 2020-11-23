39fd0bc33720bf8575c28fb94190e7e2f4f0e2b5
张志潮 <zhangzhichao@zhangzhichaodeMacBook-Air.local>
2020-11-23 18:06:46 +0800

    feat: 逻辑处理

--- src/pages/blocks/index.tsx
+++ src/pages/blocks/index.tsx
@@ -7,7 +7,7 @@ import RewardModal from '@/components/rewardModal';
 import useStage from '@/hooks/useStage';
 import useReward from '@/hooks/useReward';
 import useComponents from '@/hooks/useComponents';
-import {main_color} from '@/utils/theme';
+import { main_color } from '@/utils/theme';
 import useCreateEle, {
   ElesConfig,
   EleTypeEnums,
@@ -24,13 +24,18 @@ const sessionKey = 'optionPos';
 const Blocks: FC<PropTypes> = function(props) {
   const { visible, setVisible, onClose } = useReward();
   const answer = 'blue';
+  const colorBolcksRef = useRef([]);
+  const initColorPos = useRef({});
+  const answerBolcksRef = useRef([]);
+  const initAnswerPos = useRef({});
+
   const { stage } = useStage({
     elId: canvasId,
   });
-  const { elements, setEles, eles } = useCreateEle({
+  const { elements, setEles, eles, findElesByNames } = useCreateEle({
     stage,
   });
-  const { createHorn, createOptionsBlock } = useComponents()
+  const { createHorn, createOptionsBlock } = useComponents();
   useEffect(() => {
     initPage();
     return () => {
@@ -54,73 +59,186 @@ const Blocks: FC<PropTypes> = function(props) {
       // 占位
       ...createOptionsBlock(3, 'none'),
       // 答题色块
-      ...createOptionsColorBlock()
+      ...createOptionsColorBlock(),
     ]);
   }
   /**
    * @description 题目
    */
   function createQuestionBlocks() {
-    return ['yellow','red', 'blue','yellow','red', 'blue'].map((color, index) => {
-      const w = 79, x = 69 + (w + 23) * index
-      return {
-        type: EleTypeEnums.SPRITE,
-        option: {
-          texture: assertMap[color],
-          size: [w, 37],
-          pos: [x , 281]
-        }
-      }
-    })
+    return ['yellow', 'red', 'blue', 'yellow', 'red', 'blue'].map(
+      (color, index) => {
+        const w = 79,
+          x = 69 + (w + 23) * index;
+        return {
+          type: EleTypeEnums.SPRITE,
+          option: {
+            texture: assertMap[color],
+            size: [w, 37],
+            pos: [x, 281],
+          },
+        };
+      },
+    );
   }
   /**
    * @description 答案区域
    */
   function createAnswerBlocks() {
-    return new Array(3).fill(0).map(( _ , index) => {
-      const w = 96, x = 674 + (w + 23) * index
+    return new Array(3).fill(0).map((_, index) => {
+      const w = 96,
+        h = 58,
+        x = 674 + w / 2 + (w + 23) * index;
       return {
         type: EleTypeEnums.BLOCK,
         option: {
           name: `answer-${index}`,
-          size: [w, 58],
-          pos: [x , 270],
+          anchor: [0.5, 0.5],
+          size: [w, h],
+          pos: [x, 270 + h / 2],
           border: [2, main_color],
           borderDash: 2,
-          borderRadius: 10
-        }
-      }
-    })
+          borderRadius: 10,
+        },
+      };
+    });
   }
   /**
    * @description 创建需要拖拽的色块
    */
   function createOptionsColorBlock() {
     return ['red', 'yellow', 'blue'].map((color, index) => {
-      const w = 79, h = 37, x = 280 + w / 2 + (w + 114) * index
+      const w = 79,
+        h = 37,
+        x = 280 + w / 2 + (w + 114) * index;
       return {
         type: EleTypeEnums.SPRITE,
         option: {
-          anchor: [.5, .5],
+          anchor: [0.5, 0.5],
           name: `${color}`,
           size: [w, h],
           pos: [x, 500 + h / 2],
-          texture: assertMap[color]
+          texture: assertMap[color],
         },
-        evt: [{
-          type: EvtNameEnum.TOUCH_MOVE,
-          callback: (evt, ele) => {
-            ele.attr({
-              pos: [evt.x, evt.y]
-            })
+        evt: [
+          {
+            type: EvtNameEnum.TOUCH_MOVE,
+            callback: handleColorEleMove,
           },
-        }]
-      }
-    })
+          {
+            type: EvtNameEnum.TOUCH_END,
+            callback: handleColorEleEnd,
+          },
+        ],
+      };
+    });
   }
   useEffect(() => {
     if (!Array.isArray(elements) || elements.length === 0) return;
+    bindRef();
   }, [elements]);
+  function bindRef() {
+    colorBolcksRef.current = findElesByNames(elements, [
+      'red',
+      'yellow',
+      'blue',
+    ]);
+    answerBolcksRef.current = findElesByNames(elements, [
+      'answer-0',
+      'answer-1',
+      'answer-2',
+    ]);
+    setColorBlockInitPos();
+  }
+  /**
+   * @description 初始化各个节点的位置
+   */
+  function setColorBlockInitPos() {
+    const colorPos = {};
+    const answerPos = {};
+    colorBolcksRef.current.forEach(elm => {
+      colorPos[elm.name] = elm.attr().pos;
+    });
+    answerBolcksRef.current.forEach(elm => {
+      answerPos[elm.name] = elm.attr().pos;
+    });
+    initColorPos.current = colorPos;
+    initAnswerPos.current = answerPos;
+  }
+  /**
+   * @description 处理拖拽逻辑
+   * @param evt
+   * @param ele
+   */
+  function handleColorEleMove(evt, ele) {
+    // const curr = ele.name
+    ele.attr({
+      pos: [evt.x, evt.y],
+    });
+  }
+  /**
+   * @description 处理拖拽结束逻辑
+   * @param evt
+   * @param ele
+   */
+  function handleColorEleEnd(evt, ele) {
+    const curr = ele.name;
+    // 进入到答题区域时，将另外两个色块做动画回到
+    replaceColorBlock(ele);
+    pullRightBlock(ele);
+  }
+  /**
+   * @description 进入到答题区域时，将同位置的色块做动画回到初始位置
+   * @param ele
+   */
+  function replaceColorBlock(ele) {
+    colorBolcksRef.current.forEach(async elm => {
+      const [x, y] = ele.attr().pos;
+      const isCover = elm.isPointCollision(x, y); // 跟当前色块覆盖了
+      if (elm.name !== ele.name && isCover) {
+        const pos = initColorPos.current[elm.name];
+        await elm.animate([{ pos }], {
+          duration: 400,
+          easing: 'ease-in',
+          direction: 'alternate',
+          iterations: 1,
+          fill: 'forwards',
+        });
+      }
+    });
+  }
+  /**
+   * @description 纠正方块的位置
+   */
+  function pullRightBlock(ele) {
+    let flag = false; // 为true时，说明已经有一项匹配了
+    answerBolcksRef.current.forEach(async answerBlock => {
+      const [x, y] = ele.attr().pos;
+      const isCover = answerBlock.isPointCollision(x, y); // 跟当前色块覆盖了
+      if (isCover) {
+        flag = true
+        const pos = initAnswerPos.current[answerBlock.name];
+        await ele.animate([{ pos }], {
+          duration: 400,
+          easing: 'ease-in',
+          direction: 'alternate',
+          iterations: 1,
+          fill: 'forwards',
+        });
+        return
+      }
+      if (flag) return
+      const initPos = initColorPos.current[ele.name];
+      console.log('没覆盖到', initPos);
+      await ele.animate([{ pos: initPos }], {
+        duration: 400,
+        easing: 'ease-in',
+        direction: 'alternate',
+        iterations: 1,
+        fill: 'forwards',
+      });
+    });
+  }
   return (
     <>
       <div
