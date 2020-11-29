668d4dbea02aa309e20171b314ca362ad48a48cb
张志潮 <zhangzhichao@zhangzhichaodeMacBook-Air.local>
2020-11-29 16:16:21 +0800

    feat: 巧虎弹窗增加nextStep

--- src/components/rewardModal/index.tsx
+++ src/components/rewardModal/index.tsx
@@ -4,16 +4,27 @@
 import React, { FC, useEffect } from 'react';
 import styles from './styles/index.less';
 import Reward from '../reward';
+import {NEXT_STEP} from '@/utils/bridge';
 interface PropTypes {
   star: 1 | 2 | 3; // 评分
   visible: boolean;
   onClose?: () => void;
 }
 const ResultModal: FC<PropTypes> = function({ star, visible, onClose }) {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModel()
+    }, 5000)
+  }, [])
+  function closeModel() {
+    NEXT_STEP()
+    onClose()
+  }
+
   return (
     <>
       {visible && (
-        <div className={styles['reward_modal']} onClick={onClose}>
+        <div className={styles['reward_modal']} onClick={closeModel}>
           <Reward star={star} />
         </div>
       )}
